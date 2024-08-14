aba3188d3319c401c53e95dd61322fa37e1b6b33
oscar salamanca <salamancaoscar519@gmail.com>
2024-08-14 10:14:39 -0500

    continuación de tablas base  de datos

--- DB/copa_america.sql
+++ DB/copa_america.sql
@@ -49,3 +49,57 @@ create table selecciones(
      foreign key (asociacion)
      references asociaciones (id));
 
+
+create table partidos(
+    id int(50) primary key not null,
+    hora_partido time,
+   seleccion_1 int not null ,
+   foreign key (seleccion_1)
+   references selecciones(id),
+   seleccion_2  int not null
+   ,foreign key (seleccion_2)
+   references selecciones(id),
+   estadio_id int not null ,
+   foreign key (estadio_id)
+   references estadios(id),
+   fase_id int not null ,
+   foreign key(fase_id)
+   references fases (id));
+
+
+alter table fases 
+add primary key (id);
+
+
+
+create table personas(
+   id int (50) primary key not null,
+   nombre varchar(50),
+   apellido varchar(50),
+   fecha_nacimiento varchar(50),
+   estatura varchar(50),
+   peso varchar(50),
+   cargo varchar(50),
+   categoria varchar(50));
+
+create table posiciones(
+    id int (50) primary key not null,
+     nombre varchar(50));
+
+
+create table jugadores(
+    id int (50) primary key not null,
+    dorsal int (50),
+   	id_persona int not null,
+   	foreign key (id_persona) references personas(id),
+   	posicion_jugador int not null,
+   	foreign key(posicion_jugador) references posiciones(id),
+   	seleccion_id int not null,
+   	foreign key (seleccion_id) references selecciones(id));		  
+
+alter table personas drop column cargo;
+
+
+
+
+
